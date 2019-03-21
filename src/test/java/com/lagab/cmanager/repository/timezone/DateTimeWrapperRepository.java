0673f3b7386917a6e91563c4b611edc70fbf30bb
GABRIEL Ladislas <ladislas.gabriel.ext@humanis.com>
2019-03-21 16:51:22 +0100

    Initial application generated by JHipster-5.8.2

---
+++ src/test/java/com/lagab/cmanager/repository/timezone/DateTimeWrapperRepository.java
@@ -0,0 +1,12 @@
+package com.lagab.cmanager.repository.timezone;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data JPA repository for the DateTimeWrapper entity.
+ */
+@Repository
+public interface DateTimeWrapperRepository extends JpaRepository<DateTimeWrapper, Long> {
+
+}
