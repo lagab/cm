1d840d726ce90c15a3a23f23a93af6ad2d5b416e
lagab <gabriel.ladislas@gmail.com>
2019-04-07 02:27:16 +0200

    add exception's constructors

--- src/main/java/com/lagab/cmanager/web/rest/errors/DuplicateFileException.java
+++ src/main/java/com/lagab/cmanager/web/rest/errors/DuplicateFileException.java
@@ -10,4 +10,34 @@ public class DuplicateFileException extends AbstractThrowableProblem {
     public DuplicateFileException(String fileName) {
         super(ErrorConstants.FILE_NOT_FOUND_TYPE, fileName, Status.BAD_REQUEST);
     }
+
+    public DuplicateFileException(
+        long companyId, long repositoryId, String fileName) {
+
+        super(ErrorConstants.FILE_NOT_FOUND_TYPE,
+            String.format(
+                "{companyId=%s, repositoryId=%s, fileName=%s}", companyId,
+                repositoryId, fileName), Status.BAD_REQUEST);
+    }
+
+    public DuplicateFileException(
+        long companyId, long repositoryId, String fileName, String version) {
+
+        super(ErrorConstants.FILE_NOT_FOUND_TYPE,
+            String.format(
+                "{companyId=%s, repositoryId=%s, fileName=%s, version=%s}",
+                companyId, repositoryId, fileName, version), Status.BAD_REQUEST);
+    }
+
+    public DuplicateFileException(
+        long companyId, long repositoryId, String fileName, String version,
+        Throwable cause) {
+
+        super(ErrorConstants.FILE_NOT_FOUND_TYPE,
+            String.format(
+                "{companyId=%s, repositoryId=%s, fileName=%s, version=%s, " +
+                    "cause=%s}",
+                companyId, repositoryId, fileName, version, cause)
+            , Status.BAD_REQUEST);
+    }
 }
--- src/main/java/com/lagab/cmanager/web/rest/errors/NoSuchFileException.java
+++ src/main/java/com/lagab/cmanager/web/rest/errors/NoSuchFileException.java
@@ -10,4 +10,42 @@ public class NoSuchFileException extends AbstractThrowableProblem {
     public NoSuchFileException(String fileName) {
         super(ErrorConstants.FILE_NOT_FOUND_TYPE, fileName, Status.BAD_REQUEST);
     }
+
+    public NoSuchFileException(
+        long companyId, long repositoryId, String fileName) {
+
+        super(ErrorConstants.FILE_NOT_FOUND_TYPE,
+            String.format(
+                "{companyId=%s, repositoryId=%s, fileName=%s}", companyId,
+                repositoryId, fileName), Status.BAD_REQUEST);
+    }
+
+    public NoSuchFileException(
+        long companyId, long repositoryId, String fileName, String version) {
+
+        super(ErrorConstants.FILE_NOT_FOUND_TYPE,
+            String.format(
+                "{companyId=%s, repositoryId=%s, fileName=%s, version=%s}",
+                companyId, repositoryId, fileName, version), Status.BAD_REQUEST);
+    }
+
+    public NoSuchFileException(
+        long companyId, long repositoryId, String fileName, String version,
+        Throwable cause) {
+
+        super(ErrorConstants.FILE_NOT_FOUND_TYPE,
+            String.format(
+                "{companyId=%s, repositoryId=%s, fileName=%s, version=%s, " +
+                    "cause=%s}",
+                companyId, repositoryId, fileName, version, cause), Status.BAD_REQUEST);
+    }
+
+    public NoSuchFileException(
+        long companyId, long repositoryId, String fileName, Throwable cause) {
+
+        super(ErrorConstants.FILE_NOT_FOUND_TYPE,
+            String.format(
+                "{companyId=%s, repositoryId=%s, fileName=%s, cause=%s}",
+                companyId, repositoryId, fileName, cause), Status.BAD_REQUEST);
+    }
 }
